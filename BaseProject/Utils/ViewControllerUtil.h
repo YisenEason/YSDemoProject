0ab0d7b5ec8784ed11e40f0bb47692eb4effd7d6
xxxx_eason0yi <xxxx_eason0yi@git.cloud.tencent.com>
2018-11-15 15:12:20 +0800

    Update BaseProject/Utils/ViewControllerUtil.h

---
+++ BaseProject/Utils/ViewControllerUtil.h
@@ -0,0 +1,15 @@
+//
+//  ViewControllerUtil.h
+//  BaseProject
+//
+//  Created by eason yi on 2018/10/11.
+//  Copyright © 2018年 eason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewControllerUtil : NSObject
+
++ (UIViewController *)topViewController;
+
+@end
